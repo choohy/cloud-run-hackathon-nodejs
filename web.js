b83cb0e03be98ad0ae2a7409f4a97367f21931ce
Hwan-Yi Choo <Hwan-Yi.Choo@anz.com>
2022-09-09 13:54:13 +1000

    add movement

--- web.js
+++ web.js
@@ -8,12 +8,39 @@ app.get('/', function (req, res) {
   res.send('Let the battle begin!');
 });
 
+const gameURL = "https://cloud-run-hackathon-nodejs-w3hsbubvpa-uc.a.run.app/"
 app.post('/', function (req, res) {
   console.log(req.body);
   const moves = ['F', 'T', 'L', 'R'];
   const action = 1;
+  const arena = req.body["arena"];
+  const myLocation = arena["state"][gameURL]
+  const direction = myLocation["direction"];
+  const x = myLocation["x"];
+  const y = myLocation["y"];
 
-  
+  switch(direction) {
+    case "N":
+      if (y = 0)
+        action = 2;
+      else
+        action = 0;
+    case "S":
+      if (y = 7)
+        action = 3;
+      else
+        action = 0;
+    case "W":
+      if (x = 0)
+        action = 3
+      else
+        action = 0;
+    case "E":
+      if (x = 7)
+        action = 2;
+      else
+        action = 0;
+  }  
   // TODO add your implementation here to replace the random response
   res.send[moves[action]];
   // res.send(moves[Math.floor(Math.random() * moves.length)]);
