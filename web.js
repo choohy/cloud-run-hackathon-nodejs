4174493af67c89b7edd20cd3a09b3a3754aa7aca
Hwan-Yi Choo <Hwan-Yi.Choo@anz.com>
2022-09-09 13:37:17 +1000

    Always throw

--- web.js
+++ web.js
@@ -11,10 +11,12 @@ app.get('/', function (req, res) {
 app.post('/', function (req, res) {
   console.log(req.body);
   const moves = ['F', 'T', 'L', 'R'];
+  const action = 1;
+
   
   // TODO add your implementation here to replace the random response
-  
-  res.send(moves[Math.floor(Math.random() * moves.length)]);
+  res.send[moves[action]];
+  // res.send(moves[Math.floor(Math.random() * moves.length)]);
 });
 
 app.listen(process.env.PORT || 8080);
