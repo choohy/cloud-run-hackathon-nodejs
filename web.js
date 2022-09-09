411dc72124c54b3dfcfb25aeeb3859a3910b9a2b
Hwan-Yi Choo <Hwan-Yi.Choo@anz.com>
2022-09-09 14:51:41 +1000

    no message

--- web.js
+++ web.js
@@ -16,38 +16,108 @@ app.post('/', function (req, res) {
   const arena = req.body["arena"];
   const myLocation = arena["state"][gameURL]
   const direction = myLocation["direction"];
-  const x = myLocation["x"];
-  const y = myLocation["y"];
-  const opponents = arena["state"]
+  const myX = myLocation["x"];
+  const myY = myLocation["y"];
+  const x = 0;
+  const y = 1;
+  const forward = 0;
+  const toss = 1;
+  const left = 2;
+  const right = 3;
+  const players = arena["state"];
+  const playerLocations = [];
+  const playerLocation = [myX, myY];
+  players.forEach(element => {
+    if (element[x]!=playerLocation[0] && element[y]!=playerLocation[1])
+      playerLocations.push([element[x], element[y]]);
+  });
 
-
-
-  switch(direction) {
-    case "N":
-      if (y < 10)
-        action = 0;
-      else {
-        if (x < 1)
-          action = 3;
-        else  
-          action = 2;
+  playerLocations.forEach((opponent) => {
+    switch(direction) {
+      case "N": {
+        if (playerLocation[y] == opponent[y]) {
+          if(playerLocation[x] > opponent[x]){
+            action = toss;
+            return;
+          }
+          else {
+            action = right;
+            return;
+          }
+        }
       }
-    case "S":
-      if (y > 0)
-        action = 0;
-      else
-        action = 2;
-    case "W":
-      if (x > 0)
-        action = 0;
-      else
-        action = 2;
-    case "E":
-      if (x < 10)
-        action = 3;
-      else
-        action = 2;
-  }
+      case "E": {
+        if (playerLocation[x] == opponent[x]) {
+          if (playerLocation[y] < opponent[y]) {
+            action = toss;
+            return;
+          } else {
+            action = right;
+            return;
+          }
+        }
+      }
+      case "S": {
+        if (playerLocation[y] == opponent[y]) {
+          if (playerLocation[x] < opponent[x]) {
+            action = toss;
+            return;
+          } else {
+            action = right;
+            return;
+          }
+        }
+      }
+      case "W": {
+        if (playerLocation[x] == opponent[x]) {
+          if (playerLocation[y] > opponent[y]) {
+            action = toss;
+            return;
+          } else {
+            action = right;
+            return;
+          }
+        }
+      }
+      default: {
+        if (playerLocation[x] == 10 || playerLocation[x] == 0) {
+          action = right;
+          return;
+        } else if (playerLocation[y] == 10 || playerLocation[y] == 0) {
+          action = right;
+          return;
+        } 
+        action = forward;
+      }
+    }
+  });
+
+  // switch(direction) {
+  //   case "N":
+  //     if (y < 10)
+  //       action = 0;
+  //     else {
+  //       if (x < 1)
+  //         action = 3;
+  //       else  
+  //         action = 2;
+  //     }
+  //   case "S":
+  //     if (y > 0)
+  //       action = 0;
+  //     else
+  //       action = 2;
+  //   case "W":
+  //     if (x > 0)
+  //       action = 0;
+  //     else
+  //       action = 2;
+  //   case "E":
+  //     if (x < 10)
+  //       action = 3;
+  //     else
+  //       action = 2;
+  // }
   // TODO add your implementation here to replace the random response
   res.send[moves[action]];
   // res.send(moves[Math.floor(Math.random() * moves.length)]);
